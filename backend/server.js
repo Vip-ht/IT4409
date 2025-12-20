562a1f21a1e17194fdaa46f94c2f6be46ec85c99
Vipht <minhnguyenha22@gmail.com>
2025-12-20 17:05:23 +0700

    connected to mongoDB

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/authRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
