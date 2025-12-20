875b15b38124a60d75a6863c77e2249969172093
Vipht <minhnguyenha22@gmail.com>
2025-12-20 18:02:48 +0700

    login tạm thời

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -21,9 +21,8 @@ const UserSchema = new mongoose.Schema({
 
 // Hash password before saving
 UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
